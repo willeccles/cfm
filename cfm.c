a440ac154f9ec291db5047f3371b5b6711512c87
Will Eccles <will@eccles.dev>
2020-01-22 18:02:32 -0500

    add license header

--- cfm.c
+++ cfm.c
@@ -1,3 +1,10 @@
+/* Copyright (c) Will Eccles <will@eccles.dev>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include <dirent.h>
 #include <fcntl.h>
 #include <limits.h>
