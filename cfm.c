e7e7ebb7f07bad912c949f00d1e8b3d65752fb06
Will Eccles <will@eccles.dev>
2020-01-28 13:42:04 -0500

    sorted as all things should be

--- cfm.c
+++ cfm.c
@@ -18,9 +18,9 @@
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 // include user config
 #include "config.h"
