9aa295d5b8727c698f1d71ab36b50f4c7db80741
Will Eccles <will@eccles.dev>
2020-01-29 20:44:36 -0500

    fix thing

--- cfm.c
+++ cfm.c
@@ -662,7 +662,6 @@ static void drawscreen(char* wd, struct listelem* l, size_t n, size_t s, size_t 
     printf(" %d: %s%n", // working directory + view
         v+1, wd, &p);
 #else
-    (void)i;
     printf(" %s%n", // print working directory
         wd, &p);
 #endif
